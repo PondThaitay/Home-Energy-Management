43748d6236e7f1be6ad7a71645a515862f87d9d4
Jedsada Tiwongvorakul <pondthaitay@gmail.com>
2015-03-18 22:36:33 +0700

    add

--- app/src/main/java/www/cmsmarthome/com/Control_Process.java
+++ app/src/main/java/www/cmsmarthome/com/Control_Process.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-
 import android.util.Log;
 
 public class Control_Process {
