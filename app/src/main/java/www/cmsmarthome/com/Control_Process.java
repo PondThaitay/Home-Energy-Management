6e0b3fb10158b888857747d9b20348a8eefa220a
Jedsada Tiwongvorakul <pondthaitay@gmail.com>
2015-03-18 22:22:53 +0700

    add

--- app/src/main/java/www/cmsmarthome/com/Control_Process.java
+++ app/src/main/java/www/cmsmarthome/com/Control_Process.java
@@ -15,6 +15,7 @@ public class Control_Process {
 	private Socket socket = null;
 	private final String DEBUG_TAG = "Home Energy Management";
     private String pond;
+
 	private String StatusLamp1;
 	private String StatusLamp2;
 	private String StatusLamp3;
